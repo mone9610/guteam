c0d65ee623b0cf1768e118740d87da4be1036855
mone9610 <knemoto.prod@gmail.com>
2021-08-06 19:35:57 +0900

    First commit(develop development)

---
+++ docker-codes/http-server/teamx/src/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
