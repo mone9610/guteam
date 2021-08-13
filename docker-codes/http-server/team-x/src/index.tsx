4d4f2f956ff49dfc1bc46afba72101b40a893be3
mone9610 <knemoto.prod@gmail.com>
2021-08-13 13:43:38 +0900

    restructure react env

--- docker-codes/http-server/team-x/src/index.tsx
+++ docker-codes/http-server/team-x/src/index.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <div className="card">
-    <div className="card-body">
-    <Button variant="outline-primary">プライマリーボタン</Button>
-    </div>
-    </div>,
   </React.StrictMode>,
   document.getElementById('root')
 );
