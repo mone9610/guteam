42198b3289005a3d7b35eb09daec03a1c219eaf5
mone9610 <knemoto.prod@gmail.com>
2021-09-21 12:08:54 +0900

    その他 : railsのmodelに対するバリデーションチェックを付与

--- docker-codes/api-server/guteam/spec/models/post_spec.rb
+++ docker-codes/api-server/guteam/spec/models/post_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-    it 'factoriesのpostを参照して、レコードが登録できる' do
-        expect(FactoryBot.build(:post)).to be_valid
-    end
+  it 'factoriesのpostを参照して、レコードが登録できる' do
+    expect(FactoryBot.build(:post)).to be_valid
+  end
+  it 'factoriesのpostを参照し、messageが0文字の場合はレコードが登録できない' do
+    expect(FactoryBot.build(:post, message: '')).to_not be_valid
+  end
+  it 'factoriesのpostを参照し、messageが161文字の場合はレコードが登録できない' do
+    expect(FactoryBot.build(:post, message: 'a' * 161)).to_not be_valid
+  end
 end
