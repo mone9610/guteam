0ebbbc6db055747363b95497c4ac0070e66689e7
mone9610 <knemoto.prod@gmail.com>
2021-10-16 17:10:58 +0900

    その他:Copyright表示の修正_#32

--- docker-codes/http-server/guteam/src/container/molecules/Copyright.tsx
+++ docker-codes/http-server/guteam/src/container/molecules/Copyright.tsx
@@ -5,8 +5,8 @@ const Copyright: VFC = () => (
   <div>
     <Typography variant="body2" color="textPrimary" align="center">
       {'Copyright © '}
-      <a href="https://github.com/mone9610/guteam">mone9610 </a>
-      All Rights Reserved. {new Date().getFullYear()}.
+      <a href="https://github.com/mone9610/guteam">mone9610</a> All Rights
+      Reserved. {new Date().getFullYear()}.
     </Typography>
   </div>
 );
