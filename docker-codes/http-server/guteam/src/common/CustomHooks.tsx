2800abfbbb0c2af40da035789164a6c45d8e6162
mone9610 <knemoto.prod@gmail.com>
2021-10-17 12:13:26 +0900

    その他:不要なコメントを削除して、ReactのCICDを再起動

--- docker-codes/http-server/guteam/src/common/CustomHooks.tsx
+++ docker-codes/http-server/guteam/src/common/CustomHooks.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Theme, useMediaQuery } from '@material-ui/core';
 
-// eslint-disable-next-line import/prefer-default-export
 export const useToken = (): string => {
   const { getAccessTokenSilently } = useAuth0();
   const [token, setToken] = useState('');
